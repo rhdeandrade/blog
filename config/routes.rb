7da2b5be8b6712bc38aac3daef50eedabd4da275
Raphael de Andrade <rhdeandrade@gmail.com>
2014-04-07 22:41:52 -0300

    Not working this shit

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Blog::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'site#index'
 
+  mount Ckeditor::Engine => '/ckeditor'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
